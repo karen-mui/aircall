1a278c7074f2b34a9a95e8ef18ed144aa8921709
Karen Mui <karen_mui@hotmail.com>
2023-07-26 00:18:27 -0400

    first axios call

--- src/components/ActivityFeed.js
+++ src/components/ActivityFeed.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect , useState } from 'react';
 
 export default function ActivityFeed() {
+
+useEffect(() => {
+  axios.get(`https://cerulean-marlin-wig.cyclic.app/activities`)
+     .then(res => {
+       console.log(res.data)
+     })
+}, []);
+
   return (
     <div>ActivityFeed</div>
   )
