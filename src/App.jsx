f2718fd81647960251b16960272903229753ef05
Karen Mui <karen_mui@hotmail.com>
2023-07-25 20:06:24 -0400

    component skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client.js';
+import { useState } from 'react';
 
+// assets
 import Header from './Header.jsx';
 
-const App = () => {
+// components
+import ActivityFeed from './components/ActivityFeed.js';
+import ActivityDetail from './components/ActivityDetail.js';
+import Archive from './components/Archive.js';
+
+
+export default function App() {
+
+  const [state, setState] = useState()
+
   return (
     <div className='container'>
       <Header/>
       <div className="container-view">Some activities should be here</div>
-    </div>
+      <ActivityDetail />
+      <Archive />
+      <ActivityFeed />
+    </div> 
   );
-};
-
-ReactDOM.render(<App/>, document.getElementById('app'));
-
-export default App;
+}
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <App />
+)
