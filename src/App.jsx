b3e81225b1c2e001f532cbfe8feca1b7488d8ba1
Karen Mui <karen_mui@hotmail.com>
2023-07-26 17:22:18 -0400

    adding button feature to go to activity detail

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,8 @@ import ActivityFeed from './pages/ActivityFeed.js';
 import ActivityDetail from './pages/ActivityDetail.js';
 import Archive from './pages/Archive.js';
 
-// styles
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 export default function App() {
-
-  const [state, setState] = useState()
 
   return (
     <div className='container'>
--- src/components/ActivityItem.js
+++ src/components/ActivityItem.js
@@ -1,12 +1,18 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function ActivityItem(props) {
+
+  const click = () => {
+    console.log('i was clicked')
+  }
+
   return (
+      <ul onClick={click}>
     <ListGroup.Item>
-      Caller: {props.caller ? props.caller : "Private"}
-      Called to: {props.callee ? props.callee : "Private"}
+        Caller: {props.caller ? props.caller : "Private"}
+        Called to: {props.callee ? props.callee : "Private"}
     </ListGroup.Item>
-    );
+      </ul>
+  );
 }
